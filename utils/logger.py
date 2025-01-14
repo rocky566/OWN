189b6fe5314170e7e210af74e45e230aec2fcd77
rocky566 <rockkhanboy45@gmail.com>
2025-01-14 16:00:16 +0530

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,10 @@
+# utils/logger.py
+
+from config import LOG_FILE
+import datetime
+
+def log(message):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open(LOG_FILE, "a") as f:
+        f.write(f"[{timestamp}] {message}\n")
+    print(f"[LOG]: {message}")
